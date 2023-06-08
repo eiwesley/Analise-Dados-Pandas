3959b949233fc3b3cdbc89fceaf055c8755554c0
Wesley Medeiros <90868789+eiwesley@users.noreply.github.com>
2023-06-08 15:45:16 -0300

    :sparkles: feat: 03. Dataframe e Pandas

--- main.py
+++ main.py
@@ -3,4 +3,15 @@ import pandas as pd
 # vendas_df = pd.read_csv('src/Contoso - Cadastro Produtos.csv')
 vendas_df = pd.read_csv('src/Contoso - Vendas  - 2017.csv', sep=';')
 
-print(vendas_df)
+# print(vendas_df)
+# print(vendas_df['ID Cliente'])
+# print(vendas_df[:3])
+# print(vendas_df[['Numero da Venda', 'Data da Venda', 'ID Produto']])
+
+# vendas_df.info()
+
+# lista_clientes = vendas_df['ID Cliente']
+# print(lista_clientes)
+
+# produtos_quantidade = vendas_df[['ID Produto', 'Quantidade Vendida', 'Quantidade Devolvida']]
+# print(produtos_quantidade)
