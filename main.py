9007afbfc5d16189072bbc0383d725f93c17ca73
Wesley Medeiros <90868789+eiwesley@users.noreply.github.com>
2023-06-09 14:24:03 -0300

    :sparkles: feat: 04. Tratamento de bases e multiplas bases

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import pandas as pd
 
-# vendas_df = pd.read_csv('src/Contoso - Cadastro Produtos.csv')
-vendas_df = pd.read_csv('src/Contoso - Vendas  - 2017.csv', sep=';')
+# pd.set_option('max_columns', None)
+vendas_df = pd.read_csv(r'src/Contoso - Vendas  - 2017.csv', sep=';')
+clientes_df = pd.read_csv(r'src/Contoso - Clientes.csv', sep=';')
+produtos_df = pd.read_csv(r'src/Contoso - Cadastro Produtos.csv', sep=';')
+lojas_df = pd.read_csv(r'src/Contoso - Lojas.csv', sep=';')
+promocoes_df = pd.read_csv(r'src/Contoso - Promocoes.csv', sep=';')
 
-# print(vendas_df)
-# print(vendas_df['ID Cliente'])
-# print(vendas_df[:3])
-# print(vendas_df[['Numero da Venda', 'Data da Venda', 'ID Produto']])
+# clientes_df = clientes_df.drop(['Unnamed: 7', 'Unnamed: 8', 'Unnamed: 9', 'Unnamed: 10'], axis=1)
+# print(clientes_df)
 
-# vendas_df.info()
+clientes_df = clientes_df[['ID Cliente', 'E-mail']]
+produtos_df = produtos_df[['ID Produto', 'Categoria']]
+lojas_df = lojas_df[['ID Loja', 'Nome da Loja']]
 
-# lista_clientes = vendas_df['ID Cliente']
-# print(lista_clientes)
+vendas_df = vendas_df.merge(produtos_df, on='ID Produto')
+vendas_df = vendas_df.merge(lojas_df, on='ID Loja')
+vendas_df = vendas_df.merge(clientes_df, on='ID Cliente')
+vendas_df = vendas_df.rename(columns={'E-mail': 'E-mail do Cliente'})
 
-# produtos_quantidade = vendas_df[['ID Produto', 'Quantidade Vendida', 'Quantidade Devolvida']]
-# print(produtos_quantidade)
+print(vendas_df)
